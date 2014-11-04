3f6593bd2f8529c445bd58c5c22388b408e3defd
lpw151817 <lpw151817@qq.com>
2014-11-04 12:22:41 +0800

    添加注册判断

--- src/cm/getuptogether/activity/jerry/RegisterActivity.java
+++ src/cm/getuptogether/activity/jerry/RegisterActivity.java
@@ -8,6 +8,7 @@ import org.androidannotations.annotations.ViewsById;
 import cm.getuptogether.R;
 import cm.getuptogether.R.layout;
 import cm.getuptogether.activity.BaseActivity;
+import cm.getuptogether.request.StringRequest;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -17,14 +18,26 @@ import android.widget.EditText;
 
 @EActivity(R.layout.activity_register)
 public class RegisterActivity extends BaseActivity {
-	@ViewsById
-	EditText et_username, et_password, et_phone;
+	@ViewById
+	EditText et_username;
+	@ViewById
+	EditText et_password;
+	@ViewById
+	EditText et_phone;
 	@ViewById
 	Button bt_register;
 
 	@Click
 	void bt_register() {
-//		if(et_username.getText().toString().length()==0)
+		if (et_username.getText().toString().length() == 0)
+			showToast("«Î ‰»Î’À∫≈");
+		else if (et_password.getText().toString().length() == 0)
+			showToast("«Î ‰»Î√‹¬Î");
+		else if (et_phone.getText().toString().length() == 0)
+			showToast("«Î ‰»Î ÷ª˙∫≈");
+		else {
+			// ∑¢ÀÕ◊¢≤·«Î«Û.
+			// getVolleyTools().getQueue().add()
+		}
 	}
-
 }
